fa1aa7c003ec548f4dea72ca1f77c391629f8cf9
Nazar Dzys <ndzys@softserveinc.com>
2021-10-23 22:57:33 +0300

    basic react-router-dom setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => {
+  return <h1>Hats</h1>
+};
+
 function App() {
   return (
       <div>
-        <HomePage/>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/hats" component={HatsPage} />
+        </Switch>
       </div>
   );
 }
