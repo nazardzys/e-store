20dc9bb82b3f15ff9ae84c7af16ec54efd492ec5
Nazar Dzys <ndzys@softserveinc.com>
2021-10-25 16:56:10 +0300

    added components
    - shop page
    - collection-preview component
    - collection-item component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => {
-  return <h1>Hats</h1>
-};
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
       <div>
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route exact path="/shop/hats" component={HatsPage} />
+          <Route path="/shop" component={ShopPage} />
         </Switch>
       </div>
   );
---
+++ src/components/collection-item/collection-item.component.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ICollectionItem } from '../../interfaces/collectionItem';
+import './collection-item.styles.scss';
+
+type Props = Omit<ICollectionItem, "id">;
+
+const CollectionItem: React.FC<Props> = ({ name, price, imageUrl }) => {
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className="collection-footer">
+        <div className="name">{name}</div>
+        <div className="price">{price}</div>
+      </div>
+    </div>
+  );
+};
+
+export default CollectionItem;
