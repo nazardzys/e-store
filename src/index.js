809c34352b9c46fe19c166919ff4e6a537bafdb8
Nazar Dzys <ndzys@softserveinc.com>
2021-11-09 21:27:06 +0200

    added redux persistor

--- src/index.js
+++ src/index.js
@@ -4,16 +4,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
